bd1e65edbca1b150803ed6beb6e73e6b4625cea4
NicolasTassin <54062897+NicolasTassin@users.noreply.github.com>
2022-06-13 09:49:38 +0700

    First commit

---
+++ components/header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+
+
+const StyledDiv = styled.div`
+    display:flex;
+
+`
+const Header = () => {
+  return (
+    <StyledDiv>
+      <Image src="/fav2.ico" alt="Vercel Logo" width={80} height={50}  />
+      <h1>Webtree</h1>
+    </StyledDiv>
+  );
+};
+
+export default Header;
